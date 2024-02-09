d77f5dcebce28a060e17bdf3220c8ee0458a731a
Chafe Solomon <62526967+ChafeSolomon@users.noreply.github.com>
2024-02-09 14:52:01 -0500

    Delete terraform-artifactory directory

--- terraform-artifactory/output.tf
+++
@@ -1,9 +0,0 @@
-output "artifactory_instance_arn" {
-  value = aws_instance.artifactory_instance.arn
-}
-output "artifactory_instance_ebs" {
-  value = aws_instance.artifactory_instance.root_block_device.0.volume_id
-}
-output "openvpn_instance_ip" {
-  value = aws_instance.artifactory_openvpn.public_ip
-}
